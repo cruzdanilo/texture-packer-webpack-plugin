490ff8dc5aff2e4fe0dcf692f972191fd37fb357
danilo neves cruz <cruzdanilo@gmail.com>
2018-09-15 01:13:43 -0300

    enable hot module replacement

--- loader.js
+++ loader.js
@@ -5,7 +5,10 @@ module.exports = function loader(content) {
   const options = loaderUtils.getOptions(this) || {};
   const name = loaderUtils.interpolateName(this, '[name].[ext]', { content });
   this.emitFile(path.join(options.outputPath || '', name), content);
-  return `export default ${JSON.stringify(`#${name}`)};`;
+  return `/* ${loaderUtils.getHashDigest(content)} */
+export default ${JSON.stringify(`#${name}`)};
+if (module.hot) module.hot.decline();
+`;
 };
 
 module.exports.raw = true;
