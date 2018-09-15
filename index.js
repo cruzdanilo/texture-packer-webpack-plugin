61e943e90d822546d5d20c0f429d1dfc506716e0
danilo neves cruz <cruzdanilo@gmail.com>
2018-09-15 00:11:58 -0300

    use optimize hooks

--- index.js
+++ index.js
@@ -9,74 +9,96 @@ const plist = require('plist');
 class TexturePackerPlugin {
   constructor(options = {}) {
     this.options = options;
+    this.textures = new Map();
   }
 
   apply(compiler) {
-    compiler.hooks.emit.tapPromise('TexturePackerPlugin', async (compilation) => {
-      const { assets } = compilation;
-      const packer = new MaxRectsPacker(2048, 2048, 1, {
-        smart: true,
-        pot: false,
-        square: false,
+    compiler.hooks.thisCompilation.tap(this.constructor.name, (compilation) => {
+      const moduleTextures = {};
+
+      compilation.hooks.optimizeTree.tap(this.constructor.name, (chunks, modules) => {
+        modules.forEach(m => TexturePackerPlugin.transferTextures(moduleTextures, m.buildInfo));
       });
-      const textures = await Object.entries(assets).sort((a, b) => a[0].localeCompare(b[0]))
-        .reduce(async (promise, [file, source]) => {
-          const res = await promise;
-          if (path.extname(file) === '.png') {
-            delete assets[file];
-            const name = path.basename(file);
-            const texture = await Jimp.read(source.source());
-            packer.add(texture.bitmap.width, texture.bitmap.height, { name });
-            res[name] = texture;
-          }
-          return res;
-        }, Promise.resolve({}));
-      if (!packer.bins.length) return;
-      const atlas = new Jimp(packer.bins[0].width, packer.bins[0].height);
-      const json = { frames: {} };
-      packer.bins[0].rects.forEach((r) => {
-        json.frames[r.data.name] = {
-          spriteSize: `{${r.width},${r.height}}`,
-          spriteSourceSize: `{${r.width},${r.height}}`,
-          textureRect: `{{${r.x},${r.y}},{${r.width},${r.height}}}`,
+
+      compilation.hooks.optimizeAssets.tapPromise(this.constructor.name, async () => {
+        await this.buildAssets(compilation, moduleTextures);
+        Object.assign(compilation.assets, this.assets);
+      });
+    });
+  }
+
+  async buildAssets(compilation, newTextures = {}) {
+    TexturePackerPlugin.transferTextures(newTextures, compilation);
+    if (!Object.keys(newTextures).length) return;
+    this.results = [];
+    this.assets = {};
+    await Promise.all(Object.entries(newTextures)
+      .sort((a, b) => a[0].localeCompare(b[0]))
+      .map(async ([filename, texture]) => {
+        this.textures.set(filename, {
+          name: path.basename(filename),
+          jimp: await Jimp.read(texture.source()),
+        });
+      }));
+    const packer = new MaxRectsPacker(2048, 2048, 1, {
+      smart: true,
+      pot: false,
+      square: false,
+    });
+    this.textures.forEach(t => packer.add(t.jimp.bitmap.width, t.jimp.bitmap.height, t));
+    await Promise.all(packer.bins.map(async (bin) => {
+      const atlas = new Jimp(bin.width, bin.height);
+      const atlasInfo = { frames: {} };
+      bin.rects.forEach((rect) => {
+        const { name, jimp } = rect.data;
+        atlasInfo.frames[name] = {
+          spriteSize: `{${rect.width},${rect.height}}`,
+          spriteSourceSize: `{${rect.width},${rect.height}}`,
+          textureRect: `{{${rect.x},${rect.y}},{${rect.width},${rect.height}}}`,
         };
-        const t = textures[r.data.name];
-        t.scan(0, 0, r.width, r.height, (x, y) => {
-          atlas.setPixelColor(t.getPixelColor(x, y), x + r.x, y + r.y);
+        jimp.scan(0, 0, rect.width, rect.height, (x, y) => {
+          atlas.setPixelColor(jimp.getPixelColor(x, y), x + rect.x, y + rect.y);
         });
       });
-      await new Promise((resolve, reject) => atlas.getBuffer(Jimp.MIME_PNG, async (err, buf) => {
+      await new Promise((resolve, reject) => atlas.getBuffer(Jimp.MIME_PNG, async (err, b) => {
         if (err) reject(err);
         const outputPath = this.options.outputPath || '';
-        const png = await imagemin.buffer(buf, { use: [optipng()] });
-        const name = `atlas.${crypto.createHash('md5').update(png).digest('hex').substr(0, 6)}.png`;
-        if (name !== this.lastName) {
-          this.lastName = name;
-          const pngPath = path.join(outputPath, name);
-          assets[pngPath] = {
-            source: () => png,
-            size: () => png.length,
-          };
-          json.metadata = {
-            format: 3,
-            realTextureFileName: name,
-            textureFileName: name,
-            size: `{${atlas.bitmap.width},${atlas.bitmap.height}}`,
-          };
-          const txt = Buffer.from(plist.build(json));
-          const plistPath = path.join(
-            outputPath,
-            `atlas.${crypto.createHash('md5').update(txt).digest('hex').substr(0, 6)}.plist`,
-          );
-          assets[plistPath] = {
-            source: () => txt,
-            size: () => txt.length,
-          };
-          this.output = { png: pngPath, plist: plistPath };
-        }
+        const imageData = await imagemin.buffer(b, { use: [optipng()] });
+        const name = `atlas.${crypto.createHash('md5').update(imageData).digest('hex').substr(0, 6)}.png`;
+        const imagePath = path.join(outputPath, name);
+        atlasInfo.metadata = {
+          format: 3,
+          realTextureFileName: name,
+          textureFileName: name,
+          size: `{${atlas.bitmap.width},${atlas.bitmap.height}}`,
+        };
+        const plistData = Buffer.from(plist.build(atlasInfo));
+        const plistPath = path.join(
+          outputPath,
+          `atlas.${crypto.createHash('md5').update(plistData).digest('hex').substr(0, 6)}.plist`,
+        );
+        Object.assign(this.assets, {
+          [imagePath]: { source: () => imageData, size: () => imageData.length },
+          [plistPath]: { source: () => plistData, size: () => plistData.length },
+        });
+        this.results.push({ image: imagePath, plist: plistPath });
         resolve();
       }));
+    }));
+  }
+
+  static transferTextures(target, buildInfo) {
+    if (!buildInfo.assets) return;
+    Object.entries(buildInfo.assets).forEach(([filename, asset]) => {
+      if (!TexturePackerPlugin.isTexture(filename, asset)) return;
+      const { assets } = buildInfo;
+      delete assets[filename];
+      Object.assign(target, { [filename]: asset });
     });
+  }
+
+  static isTexture(filename, asset) {
+    return asset && path.extname(filename) === '.png';
   }
 
   static loader(options) {
